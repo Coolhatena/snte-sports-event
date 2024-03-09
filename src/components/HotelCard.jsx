4274b396417a0e77433a9dc115947a7dc41853ea
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2024-03-09 12:24:08 -0800

    Progress on hotel cards

---
+++ src/components/HotelCard.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './styles/HotelCard.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot, faPhone, faGlobe } from '@fortawesome/free-solid-svg-icons'
+
+// TODO: ADD ICONS FOR THE WEBSITE, LOCATIONS AND PHONE SECTIONS
+const HotelCard = ({title, image, web, location, phone_number, description}) => {
+  return (
+	<div className='card-container'>
+		<div className='card-image-title' style={{backgroundImage: `url(img/${image})`}}>
+			<div className='card-image-title-content'>
+				<h3 className='card-title'>{title}</h3>
+			</div>
+		</div>
+		<div className='card-content'>
+			<div className='card-info-container'>
+				<FontAwesomeIcon icon={faGlobe} className='card-icon'/>
+				<a className='card-website' href={web}>{web}</a>
+			</div>
+			<div className='card-info-container'>
+				<FontAwesomeIcon icon={faLocationDot} className='card-icon'/>
+				<p className='card-location'>{location}</p>
+			</div>
+
+			<div className='card-info-container'>
+				<FontAwesomeIcon icon={faPhone} className='card-icon'/>
+				<p className='card-phone'>{phone_number}</p>
+			</div>
+			<p className='card-description'>{description}</p>
+		</div>
+	</div>
+  )
+}
+
+export default HotelCard
