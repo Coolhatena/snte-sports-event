d15fed72df77248bb2000742c106674a07727886
ripOzziel <ozielinhov@gmail.com>
2024-03-10 00:24:34 -0800

    Update main page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 const Header = () => {
   return (
 	<div className='container'>
-		<h1 className='title'>SNTE</h1>
+		<Link href='/' className='title'>SNTE</Link>
 		<Link href='#' className='button'>Convocatoria</Link>
 		<Link href='#' className='button'>Reglamento Deportivo</Link>
 		<Link href='/hotels' className='button'>Hoteles</Link>
