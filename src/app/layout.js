960416f9220624955affcce8c26d37d510f8d647
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2024-03-09 21:46:01 -0800

    Set fixed header on navigation

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import Header from '@/components/Header';
+
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
@@ -15,7 +17,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+    	<body className={inter.className}>
+			<Header />	
+			{children}
+		</body>
     </html>
   );
 }
