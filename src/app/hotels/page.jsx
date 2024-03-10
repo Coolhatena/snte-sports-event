4b82dcedd61c7d786d222553775d0ccbe8d6289c
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2024-03-09 21:50:51 -0800

    Create Hotels page

---
+++ src/app/hotels/page.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+import HotelCard from '@/components/HotelCard'
+
+const Hotels = () => {
+  return (
+	<div>
+		<HotelCard 
+			title={'Villa Marina'}
+			image={'villamarina.webp'}
+			web={'https://villamarinabc.com/es?currency=MXN'}
+			location={'Av Adolfo López Mateos S/N, Zona Centro, 22800 Ensenada, B.C.'}
+			phone_number={'646 178 3351'}
+			description={`Este moderno hotel se encuentra a 5 minutos a pie del Museo de Historia y a 5 km de la Universidad Autónoma de Baja California. 
+							Las habitaciones son sencillas y coloridas, y cuentan con Wi-Fi gratis y TV satelital. Las habitaciones mejoradas tienen sofá cama. Se ofrece servicio a la habitación.
+							El estacionamiento es gratuito. Otras comodidades incluyen una piscina al aire libre con solárium, jardines paisajísticos, un restaurante informal y un bar.
+							Wi-Fi gratis, Estacionamiento gratuito, Accesible, Piscina al aire libre, Aire acondicionado, Servicio de lavandería etc.`
+						}
+		/>
+		
+		<HotelCard 
+			title={'Villa Marina'}
+			image={'villamarina.webp'}
+			web={'https://villamarinabc.com/es?currency=MXN'}
+			location={'Av Adolfo López Mateos S/N, Zona Centro, 22800 Ensenada, B.C.'}
+			phone_number={'646 178 3351'}
+			description={`Este moderno hotel se encuentra a 5 minutos a pie del Museo de Historia y a 5 km de la Universidad Autónoma de Baja California. 
+							Las habitaciones son sencillas y coloridas, y cuentan con Wi-Fi gratis y TV satelital. Las habitaciones mejoradas tienen sofá cama. Se ofrece servicio a la habitación.
+							El estacionamiento es gratuito. Otras comodidades incluyen una piscina al aire libre con solárium, jardines paisajísticos, un restaurante informal y un bar.
+							Wi-Fi gratis, Estacionamiento gratuito, Accesible, Piscina al aire libre, Aire acondicionado, Servicio de lavandería etc.`
+						}
+		/>
+
+		<HotelCard 
+			title={'Villa Marina'}
+			image={'villamarina.webp'}
+			web={'https://villamarinabc.com/es?currency=MXN'}
+			location={'Av Adolfo López Mateos S/N, Zona Centro, 22800 Ensenada, B.C.'}
+			phone_number={'646 178 3351'}
+			description={`Este moderno hotel se encuentra a 5 minutos a pie del Museo de Historia y a 5 km de la Universidad Autónoma de Baja California. 
+							Las habitaciones son sencillas y coloridas, y cuentan con Wi-Fi gratis y TV satelital. Las habitaciones mejoradas tienen sofá cama. Se ofrece servicio a la habitación.
+							El estacionamiento es gratuito. Otras comodidades incluyen una piscina al aire libre con solárium, jardines paisajísticos, un restaurante informal y un bar.
+							Wi-Fi gratis, Estacionamiento gratuito, Accesible, Piscina al aire libre, Aire acondicionado, Servicio de lavandería etc.`
+						}
+		/>
+	</div>
+  )
+}
+
+export default Hotels
