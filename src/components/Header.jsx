ccb87a081d63ae879a6c6512a7c0bd5cdaa0b19c
mayra1703 <111815136+mayra1703@users.noreply.github.com>
2024-03-10 04:23:07 -0700

    Update navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 import Image from 'next/image'
 import './styles/header.css'
@@ -5,18 +7,40 @@ import './styles/header.css'
 import Link from 'next/link'
 
 const Header = () => {
-  return (
+	const toogleMenu = () => {
+		const linksContainer = document.querySelector('.linksContainer');
 
-	<nav className='bg-blueIte'>
-		<div className='container'>
-			<a href='https://snte.org.mx' className="flex items-center space-x-3 rtl:space-x-reverse">
-				<Image src='/img/Logo_Snte_Blanco.png' className='logo' width={180} height={50} alt='Logo'/>
+		if(linksContainer.style.display === 'none'){
+			linksContainer.style.display = 'flex';
+		}
+
+		else{
+			linksContainer.style.display = 'none';
+		}
+	}
+
+  return (
+	<div className='container'>
+		<section className='navbar'>
+			<a href='https://snte.org.mx' className="containerLogo">
+				<Image src='/img/Logo_Snte_Blanco.png' width={180} height={50} alt='Logo'/>
 			</a>
-			<Link href='#' className='button'>Convocatoria</Link>
-			<Link href='#' className='button'>Reglamento Deportivo</Link>
-			<Link href='/hotels' className='button'>Hoteles</Link>
-		</div>
-	</nav>
+
+			<div className='mobileMenuButton' onClick={toogleMenu}>
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill='#ffff'>
+					<path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/>
+				</svg>
+			</div>
+		</section>
+		
+		<section className='options'>
+			<div className='linksContainer'>
+				<Link href='#' className='button'>Convocatoria</Link>
+				<Link href='#' className='button'>Reglamento Deportivo</Link>
+				<Link href='/hotels' className='button'>Hoteles</Link>
+			</div>
+		</section>
+	</div>
   )
 }
 
