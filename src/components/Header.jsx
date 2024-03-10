fe1b55c1bec73afb71fd3b66d6f77bdf66969131
mayra1703 <111815136+mayra1703@users.noreply.github.com>
2024-03-10 04:50:28 -0700

    Add Link Home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,6 +35,7 @@ const Header = () => {
 		
 		<section className='options'>
 			<div className='linksContainer'>
+				<Link href='#' className='button'>Home</Link>
 				<Link href='#' className='button'>Convocatoria</Link>
 				<Link href='#' className='button'>Reglamento Deportivo</Link>
 				<Link href='/hotels' className='button'>Hoteles</Link>
