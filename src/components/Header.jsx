e1da3cee0c0d2a0546810b7e240a84dd99cb2e65
mayra1703 <111815136+mayra1703@users.noreply.github.com>
2024-03-10 01:09:26 -0800

    Link logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
+import Image from 'next/image'
 import './styles/header.css'
 
 import Link from 'next/link'
 
 const Header = () => {
   return (
-	<div className='container'>
-		<h1 className='title'>SNTE</h1>
-		<Link href='#' className='button'>Convocatoria</Link>
-		<Link href='#' className='button'>Reglamento Deportivo</Link>
-		<Link href='/hotels' className='button'>Hoteles</Link>
-	</div>
+	<nav className='bg-blueIte'>
+		<div className='container'>
+			<a href='https://snte.org.mx' className="flex items-center space-x-3 rtl:space-x-reverse">
+				<Image src='/img/Logo_Snte_Blanco.png' className='logo' width={180} height={50} alt='Logo'/>
+			</a>
+			<Link href='#' className='button'>Convocatoria</Link>
+			<Link href='#' className='button'>Reglamento Deportivo</Link>
+			<Link href='/hotels' className='button'>Hoteles</Link>
+		</div>
+	</nav>
   )
 }
 
