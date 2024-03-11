f4ff51a56d8219641493341c0fade57d566f6a29
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2024-03-10 18:01:46 -0700

    Set default page as main

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,7 @@
 import Header from '@/components/Header';
 import HotelCard from '@/components/HotelCard';
-import Image from "next/image";
-import Main from '@/components/Main';
-const imgPath = '/img/zyro-snte.png'  
+import { redirect } from 'next/navigation';
+ 
 export default function Home() {
-  return (
-    <main>
-      <Image
-          src={'/img/zyro-snte.png'}
-          alt="SNTE seccion 2"
-          layout='responsive'
-          width={1300}
-          height={200}
-        />
-      <Main/>
-    </main>
-  );
+	redirect('/main')
 }
