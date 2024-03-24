b68d5bbfca568367adede33f4509bf08ab4e5fab
mayra1703 <111815136+mayra1703@users.noreply.github.com>
2024-03-24 16:48:30 -0700

    add contact us section

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './page.css'
+
+const Contact = () => {
+  return (
+	<div className='contact-container'>
+        <div className='flex flex-col'>
+            <h1>Contact Us</h1>
+            <p>Puedes contactarnos en la siguiente direccion</p>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Contact
