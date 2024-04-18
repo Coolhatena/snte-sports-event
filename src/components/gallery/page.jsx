8dc4996c5df815a1bb7c02b5684765ee59681c18
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2024-04-17 23:34:30 -0700

    Remove misplaced p tag

--- src/components/gallery/page.jsx
+++ src/components/gallery/page.jsx
@@ -8,7 +8,6 @@ const Gallery = () => {
             <div className='header'>
                 <h1>Galería</h1>
                 <p>Para más fotos sobre el Prenacional Deportivo Ensenada 2024, visita la siguiente galería</p>
-                <p>fecha limite para el preregistro: 15 de junio</p>
                 <a className="enlace" href="https://sites.google.com/Itlp.edu.mx/prenacionaldeportivolapaz2023">Galería de fotos</a>
             </div>
             
