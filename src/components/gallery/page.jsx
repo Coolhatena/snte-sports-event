98813fcefb340d607a3bfff0d599fc6afa8c6798
mayra1703 <111815136+mayra1703@users.noreply.github.com>
2024-06-24 16:13:55 -0700

    Add gallery & link drive

--- src/components/gallery/page.jsx
+++ src/components/gallery/page.jsx
@@ -7,12 +7,21 @@ const Gallery = () => {
         <div className='gallery-container'>
             <div className='header'>
                 <h1>Galería</h1>
-                <p>Para más fotos sobre el Prenacional Deportivo Ensenada 2024, visita la siguiente galería</p>
-                <a className="enlace" href="/">Galería de fotos</a>
+                <p>Para más fotos sobre el Prenacional Deportivo Ensenada 2024, visita la siguiente <a className="enlace" href="https://drive.google.com/drive/folders/1MuT_1r0soq5TwRuchRedCj0RsiqJtM-y">Galería de fotos</a></p>
             </div>
             
-            <div className='img'>
+            <div className='img-container'>
+                <img src='/img/img01.jpg'></img>
                 
+                <img src='/img/img02.jpg'></img>
+                
+                <img src='/img/img03.jpg'></img>
+                
+                <img src='/img/img04.jpg'></img>
+                
+                <img src='/img/img05.jpg'></img>
+                
+                <img src='/img/img06.jpg'></img>
             </div>
         </div>
     )
