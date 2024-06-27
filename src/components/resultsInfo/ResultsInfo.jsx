666f5da5e0f67646ac60d1d4fe54a23965dd36e2
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2024-06-27 03:07:08 -0700

    Create logo index

--- src/components/resultsInfo/ResultsInfo.jsx
+++ src/components/resultsInfo/ResultsInfo.jsx
@@ -8,6 +8,7 @@ import { ResultDay } from '../resultDay/ResultDay';
 import { TableChess } from '../tableChess/TableChess';
 import { ResultTable } from '../resultTable/ResultTable';
 import { ResultDiscipline } from '../resultDiscipline/ResultDiscipline';
+import { logos } from '@/helpers/logos';
 
 export const ResultsInfo = () => {
 	const [selectedDay, setSelectedDay] = useState('day1');
@@ -15,6 +16,7 @@ export const ResultsInfo = () => {
 	const handleDayChange = (day) => {
 		setSelectedDay(day);
 	};
+
 
 	return (
 		<div>
@@ -24,27 +26,27 @@ export const ResultsInfo = () => {
 				{selectedDay === 'day1' && (
 					<ResultDay title={"Resultados - día 1"}>
 						<ResultDiscipline title={"Disciplina Softbol"}>
-							<ResultTable logo1={"/img/logos-tecnm/logo-mazatlan.png"} team1={"Mazatlán"} score1={"13"} team2={"Nogales"} score2={"3"} logo2={"/img/logos-tecnm/logo-nogales.png"} />
+							<ResultTable logo1={logos.mazatlan} team1={"Mazatlán"} score1={"13"} team2={"Nogales"} score2={"3"} logo2={logos.nogales} />
 
-							<ResultTable logo1={"/img/logos-tecnm/logo-guaymas.png"} team1={"Guaymas"} score1={"11"} team2={"Tijuana"} score2={"0"} logo2={"/img/logos-tecnm/logo-tijuana.png"} />
+							<ResultTable logo1={logos.guaymas} team1={"Guaymas"} score1={"11"} team2={"Tijuana"} score2={"0"} logo2={logos.tijuana} />
 						</ResultDiscipline>
 
 						<ResultDiscipline title={"Disciplina Fútbol Soccer"}>
-							<ResultTable logo1={"/img/logos-tecnm/logo-ensenada.png"} team1={"Ensenada"} score1={"6"} team2={"Huatabampo"} score2={"1"} logo2={"/img/logos-tecnm/logo-huatabampo.png"} />
+							<ResultTable logo1={logos.ensenada} team1={"Ensenada"} score1={"6"} team2={"Huatabampo"} score2={"1"} logo2={logos.huatabampo} />
 
-							<ResultTable logo1={"/img/logos-tecnm/logo-culiacan.png"} team1={"Culiacán"} score1={"2"} team2={"La Paz"} score2={"0"} logo2={"/img/logos-tecnm/logo-la-paz.png"} />
+							<ResultTable logo1={logos.culiacan} team1={"Culiacán"} score1={"2"} team2={"La Paz"} score2={"0"} logo2={logos.laPaz} />
 						</ResultDiscipline>
 
 						<ResultDiscipline title={"Disciplina Voleibol Femenil"}>
-							<ResultTable logo1={"/img/logos-tecnm/logo-ensenada.png"} team1={"Ensenada"} score1={"2"} team2={"Nogales"} score2={"0"} logo2={"/img/logos-tecnm/logo-nogales.png"} />
+							<ResultTable logo1={logos.ensenada} team1={"Ensenada"} score1={"2"} team2={"Nogales"} score2={"0"} logo2={logos.nogales} />
 
-							<ResultTable logo1={"/img/logos-tecnm/logo-la-paz.png"} team1={"La Paz"} score1={"2"} team2={"Mexicali"} score2={"1"} logo2={"/img/logos-tecnm/logo-mexicali.png"} />
+							<ResultTable logo1={logos.laPaz} team1={"La Paz"} score1={"2"} team2={"Mexicali"} score2={"1"} logo2={logos.mexicali} />
 						</ResultDiscipline>
 
 						<ResultDiscipline title={"Disciplina Básquetbol"}>
-							<ResultTable logo1={"/img/logos-tecnm/logo-la-paz.png"} team1={"La Paz"} score1={"85"} team2={"Agua Prieta"} score2={"19"} logo2={"/img/logos-tecnm/logo-agua-prieta.png"} />
+							<ResultTable logo1={logos.laPaz} team1={"La Paz"} score1={"85"} team2={"Agua Prieta"} score2={"19"} logo2={logos.aguaPrieta} />
 
-							<ResultTable logo1={"/img/logos-tecnm/logo-nogales.png"} team1={"Nogales"} score1={"44"} team2={"Mexicali"} score2={"18"} logo2={"/img/logos-tecnm/logo-mexicali.png"} />
+							<ResultTable logo1={logos.nogales} team1={"Nogales"} score1={"44"} team2={"Mexicali"} score2={"18"} logo2={logos.mexicali} />
 						</ResultDiscipline>
 
 						<ResultDiscipline title={"Disciplina Ajedrez"}>
@@ -65,45 +67,45 @@ export const ResultsInfo = () => {
 
 					<ResultDay title={"Resultados - día 2"} >
 						<ResultDiscipline title={"Disciplina Softbol"}>
-							<ResultTable logo1={"/img/logos-tecnm/logo-mazatlan.png"} team1={"Mazatlán"} score1={"35"} team2={"Hermosillo"} score2={"3"} logo2={"/img/logos-tecnm/logo-hermosillo.png"} />
+							<ResultTable logo1={logos.mazatlan} team1={"Mazatlán"} score1={"35"} team2={"Hermosillo"} score2={"3"} logo2={logos.hermosillo} />
 
-							<ResultTable logo1={"/img/logos-tecnm/logo-nogales.png"} team1={"Nogales"} score1={"15"} team2={"Tijuana"} score2={"8"} logo2={"/img/logos-tecnm/logo-tijuana.png"} />
+							<ResultTable logo1={logos.nogales} team1={"Nogales"} score1={"15"} team2={"Tijuana"} score2={"8"} logo2={logos.tijuana} />
 						</ResultDiscipline>
 
 						<ResultDiscipline title={"Disciplina Fútbol Soccer"}>
-							<ResultTable logo1={"/img/logos-tecnm/logo-ensenada.png"} team1={"Ensenada"} score1={"1"} team2={"Mazatlán"} score2={"1"} logo2={"/img/logos-tecnm/logo-mazatlan.png"} />
+							<ResultTable logo1={logos.ensenada} team1={"Ensenada"} score1={"1"} team2={"Mazatlán"} score2={"1"} logo2={logos.mazatlan} />
 
-							<ResultTable logo1={"/img/logos-tecnm/logo-culiacan.png"} team1={"Culiacán"} score1={"6"} team2={"Tijuana"} score2={"0"} logo2={"/img/logos-tecnm/logo-tijuana.png"} />
+							<ResultTable logo1={logos.culiacan} team1={"Culiacán"} score1={"6"} team2={"Tijuana"} score2={"0"} logo2={logos.tijuana} />
 							
-							<ResultTable logo1={"/img/logos-tecnm/logo-mazatlan.png"} team1={"Mazatlán"} score1={"7"} team2={"Huatabampo"} score2={"1"} logo2={"/img/logos-tecnm/logo-huatabampo.png"} />
+							<ResultTable logo1={logos.mazatlan} team1={"Mazatlán"} score1={"7"} team2={"Huatabampo"} score2={"1"} logo2={logos.huatabampo} />
 
-							<ResultTable logo1={"/img/logos-tecnm/logo-tijuana.png"} team1={"Tijuana"} score1={"1"} team2={"La Paz"} score2={"1"} logo2={"/img/logos-tecnm/logo-la-paz.png"} />
+							<ResultTable logo1={logos.tijuana} team1={"Tijuana"} score1={"1"} team2={"La Paz"} score2={"1"} logo2={logos.laPaz} />
 						</ResultDiscipline>
 
 						<ResultDiscipline title={"Disciplina Voleibol Femenil"}>
-							<ResultTable logo1={"/img/logos-tecnm/logo-la-paz.png"} team1={"La Paz"} score1={"2"} team2={"Hermosillo"} score2={"0"} logo2={"/img/logos-tecnm/logo-hermosillo.png"} />
+							<ResultTable logo1={logos.laPaz} team1={"La Paz"} score1={"2"} team2={"Hermosillo"} score2={"0"} logo2={logos.hermosillo} />
 
-							<ResultTable logo1={"/img/logos-tecnm/logo-culiacan.png"} team1={"Culiacán"} score1={"2"} team2={"Nogales"} score2={"1"} logo2={"/img/logos-tecnm/logo-nogales.png"} />
+							<ResultTable logo1={logos.culiacan} team1={"Culiacán"} score1={"2"} team2={"Nogales"} score2={"1"} logo2={logos.nogales} />
 
-							<ResultTable logo1={"/img/logos-tecnm/logo-hermosillo.png"} team1={"Hermosillo"} score1={"2"} team2={"Mexicali"} score2={"1"} logo2={"/img/logos-tecnm/logo-mexicali.png"} />
+							<ResultTable logo1={logos.hermosillo} team1={"Hermosillo"} score1={"2"} team2={"Mexicali"} score2={"1"} logo2={logos.mexicali} />
 
-							<ResultTable logo1={"/img/logos-tecnm/logo-culiacan.png"} team1={"Culiacán"} score1={"2"} team2={"Ensenada"} score2={"0"} logo2={"/img/logos-tecnm/logo-ensenada.png"} />
+							<ResultTable logo1={logos.culiacan} team1={"Culiacán"} score1={"2"} team2={"Ensenada"} score2={"0"} logo2={logos.ensenada} />
 						</ResultDiscipline>
 
 						<ResultDiscipline title={"Disciplina Voleibol Varonil"}>
-							<ResultTable logo1={"/img/logos-tecnm/logo-culiacan.png"} team1={"Culiacán"} score1={"2"} team2={"Mexicali"} score2={"0"} logo2={"/img/logos-tecnm/logo-mexicali.png"} />
+							<ResultTable logo1={logos.culiacan} team1={"Culiacán"} score1={"2"} team2={"Mexicali"} score2={"0"} logo2={logos.mexicali} />
 
-							<ResultTable logo1={"/img/logos-tecnm/logo-huatabampo.png"} team1={"Huatabampo"} score1={"2"} team2={"La Paz"} score2={"0"} logo2={"/img/logos-tecnm/logo-la-paz.png"} />
+							<ResultTable logo1={logos.huatabampo} team1={"Huatabampo"} score1={"2"} team2={"La Paz"} score2={"0"} logo2={logos.laPaz} />
 
-							<ResultTable logo1={"/img/logos-tecnm/logo-culiacan.png"} team1={"Culiacán"} score1={"2"} team2={"Nogales"} score2={"1"} logo2={"/img/logos-tecnm/logo-nogales.png"} />
+							<ResultTable logo1={logos.culiacan} team1={"Culiacán"} score1={"2"} team2={"Nogales"} score2={"1"} logo2={logos.nogales} />
 
-							<ResultTable logo1={"/img/logos-tecnm/logo-la-paz.png"} team1={"La Paz"} score1={"2"} team2={"Ensenada"} score2={"1"} logo2={"/img/logos-tecnm/logo-ensenada.png"} />
+							<ResultTable logo1={logos.laPaz} team1={"La Paz"} score1={"2"} team2={"Ensenada"} score2={"1"} logo2={logos.ensenada} />
 						</ResultDiscipline>
 
 						<ResultDiscipline title={"Disciplina Básquetbol"}>
-							<ResultTable logo1={"/img/logos-tecnm/logo-nogales.png"} team1={"Nogales"} score1={"53"} team2={"Hermosillo"} score2={"20"} logo2={"/img/logos-tecnm/logo-hermosillo.png"} />
+							<ResultTable logo1={logos.nogales} team1={"Nogales"} score1={"53"} team2={"Hermosillo"} score2={"20"} logo2={logos.hermosillo} />
 
-							<ResultTable logo1={"/img/logos-tecnm/logo-la-paz.png"} team1={"La Paz"} score1={"38"} team2={"Tijuana"} score2={"30"} logo2={"/img/logos-tecnm/logo-tijuana.png"} />
+							<ResultTable logo1={logos.laPaz} team1={"La Paz"} score1={"38"} team2={"Tijuana"} score2={"30"} logo2={logos.tijuana} />
 						</ResultDiscipline>
 					</ResultDay>
 				)}
---
+++ src/helpers/logos.js
@@ -0,0 +1,13 @@
+export const logos = {
+	mazatlan:	"/img/logos-tecnm/logo-mazatlan.png",
+	guaymas:	"/img/logos-tecnm/logo-guaymas.png",
+	nogales:	"/img/logos-tecnm/logo-nogales.png",
+	tijuana:	"/img/logos-tecnm/logo-tijuana.png",
+	huatabampo:	"/img/logos-tecnm/logo-huatabampo.png",
+	ensenada:	"/img/logos-tecnm/logo-ensenada.png",
+	culiacan:	"/img/logos-tecnm/logo-culiacan.png",
+	laPaz:		"/img/logos-tecnm/logo-la-paz.png",
+	aguaPrieta:	"/img/logos-tecnm/logo-agua-prieta.png",
+	mexicali:	"/img/logos-tecnm/logo-mexicali.png",
+	hermosillo:	"/img/logos-tecnm/logo-hermosillo.png"
+}
