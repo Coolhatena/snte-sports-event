7da70e733af1e85dbde2793f96b213934e986f5b
mayra1703 <111815136+mayra1703@users.noreply.github.com>
2024-06-26 22:39:10 -0700

    table day 1

--- src/components/resultsInfo/ResultsInfo.jsx
+++ src/components/resultsInfo/ResultsInfo.jsx
@@ -72,22 +72,27 @@ export const ResultsInfo = () => {
 
 			<ResultDay title={"Resultados - dia 1"}>
 				<ResultDiscipline title={"Disciplina Softbol"}>
-					<ResultTable logo1={"/img/logos-tecnm/logo-mazatlan.jpg"} team1={"Mazatlán"} score1={"13"} team2={"Nogales"} score2={"3"} logo2={"/img/logos-tecnm/logo-nogales.jpg"} />
+					<ResultTable logo1={"/img/logos-tecnm/logo-mazatlan.png"} team1={"Mazatlán"} score1={"13"} team2={"Nogales"} score2={"3"} logo2={"/img/logos-tecnm/logo-nogales.png"} />
 
-					<ResultTable logo1={"/img/logos-tecnm/logo-guaymas.jpg"} team1={"Guaymas"} score1={"11"} team2={"Tijuana"} score2={"0"} logo2={"/img/logos-tecnm/logo-tijuana.png"} />
-					
+					<ResultTable logo1={"/img/logos-tecnm/logo-guaymas.png"} team1={"Guaymas"} score1={"11"} team2={"Tijuana"} score2={"0"} logo2={"/img/logos-tecnm/logo-tijuana.png"} />
 				</ResultDiscipline>
+
 				<ResultDiscipline title={"Disciplina Futbol Soccer"}>
-					<h4>Ensenada 6 - Huatabampo 1</h4>
-					<h4>Culiacan 2 - La Paz 0</h4>
+					<ResultTable logo1={"/img/logos-tecnm/logo-ensenada.png"} team1={"Ensenada"} score1={"6"} team2={"Huatabampo"} score2={"1"} logo2={"/img/logos-tecnm/logo-huatabampo.png"} />
+
+					<ResultTable logo1={"/img/logos-tecnm/logo-culiacan.png"} team1={"Culiacan"} score1={"2"} team2={"La Paz"} score2={"0"} logo2={"/img/logos-tecnm/logo-la-paz.png"} />
 				</ResultDiscipline>
+
 				<ResultDiscipline title={"Disciplina Voleibol Femenil"}>
-					<h4>Ensenada 2 - Nogales 0</h4>
-					<h4>La Paz 2 - Mexicali 1</h4>
+					<ResultTable logo1={"/img/logos-tecnm/logo-ensenada.png"} team1={"Ensenada"} score1={"2"} team2={"Nogales"} score2={"0"} logo2={"/img/logos-tecnm/logo-nogales.png"} />
+
+					<ResultTable logo1={"/img/logos-tecnm/logo-la-paz.png"} team1={"La Paz"} score1={"2"} team2={"Mexicali"} score2={"1"} logo2={"/img/logos-tecnm/logo-mexicali.png"} />
 				</ResultDiscipline>
+
 				<ResultDiscipline title={"Disciplina Basquetbol"}>
-					<h4>La Paz 85 - Agua Prieta 19</h4>
-					<h4>Nogales 44 - Mexicali 18</h4>
+					<ResultTable logo1={"/img/logos-tecnm/logo-la-paz.png"} team1={"La Paz"} score1={"85"} team2={"Agua Prieta"} score2={"19"} logo2={"/img/logos-tecnm/logo-agua-prieta.png"} />
+
+					<ResultTable logo1={"/img/logos-tecnm/logo-nogales.png"} team1={"Nogales"} score1={"44"} team2={"Mexicali"} score2={"18"} logo2={"/img/logos-tecnm/logo-mexicali.png"} />
 				</ResultDiscipline>
 			</ResultDay>
 
